a5e670b7bb25e69b25a7a0e2093c4fa312b3a01d
Jason Brown <jasontbrown99@gmail.com>
2017-01-20 11:43:55 -0800

    refactor vericifaction process under one function

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 // validates user input as a number
 var validateInput = function(number) {
-  if (isNaN(number)) {
+  if (isNaN(number) || (number % 1 !== 0)) {
   return alert("Please enter an integer, starting with 1.");
   } else {
     return number
   }
 };
 
-var integer = function(number) {
-  if (number % 1 !== 0) {
-  return alert("Please enter a whole number")
-  } else {
-    return number
-  }
-};
+// var integer = function(number) {
+//   if (number % 1 !== 0) {
+//   return alert("Please enter a whole number")
+//   } else {
+//     return number
+//   }
+// };
 
 
 // var numberSplitter = function(number) {
