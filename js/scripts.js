292c2c62659f8bb9e4799427355fc0c09a4b5d34
Jason Brown <jasontbrown99@gmail.com>
2017-01-20 11:42:06 -0800

    add verification rule to check if input is an integer

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,48 @@
-var validateInput = function(numVar) {
-  if (isNaN(numVar)) {
-  return alert("Please enter a number.");
+// validates user input as a number
+var validateInput = function(number) {
+  if (isNaN(number)) {
+  return alert("Please enter an integer, starting with 1.");
   } else {
-    return numVar
+    return number
   }
 };
+
+var integer = function(number) {
+  if (number % 1 !== 0) {
+  return alert("Please enter a whole number")
+  } else {
+    return number
+  }
+};
+
+
+// var numberSplitter = function(number) {
+//   return verified.split("").map(function(t){return parseInt(t)});
+// }
+
+
+
+
+$(document).ready(function() {
+  $("form#game").submit(function(event) {
+    event.preventDefault();
+    var number = $("input#number").val();
+    var verified = validateInput(number);
+    var wholeNumber = integer(number);
+    // var numbers = numberSplitter(verified);
+    // console.log(validateInput);
+  });
+});
+    // var numbers = numberSplitter(verified);
+    // var onesPlace = onesIdentifier(numbers);
+    // var tensPlace = tensIdentifier(numbers);
+    // console.log(onesPlace);
+  //  var hundredsPlace = hundredsIdentifier(numbers);
+  //  var thousandsPlace = thousandsIdentifier(numbers);
+//   console.log(thousandsPlace + hundredsPlace + tensPlace + onesPlace);
+  //  var result = roman(thousandsPlace, hundredsPlace, tensPlace, onesPlace);
+  //  console.log(result);
+    // $(".number").text(number);
+    // $(".roman").text(result);
+    //
+    // $("#result").show();
