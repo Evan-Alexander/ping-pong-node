92a0ef44aa45daeda40c23892f7176277e058862
Jason Brown <jasontbrown99@gmail.com>
2017-01-25 19:29:46 -0800

    change approach to pingpong game.  It will now populate an array. Move html element to show list whithin well.

--- js/scripts.js
+++ js/scripts.js
@@ -1,75 +1,36 @@
-// validates user input as a number and an integer.
-var validateInput = function(number) {
-  if (isNaN(number) || (number % 1 !== 0)) {
-  return alert("Please enter an integer, starting with 1.");
-  } else {
-    return number
+var numberList = function(number) {
+  var i = [];
+  for ( var i=1; i < number; i++ ) {
+    console.log(i)
   }
-};
+  return i;
+}
 
-// rule for ping
 
-// rule for pong
-// var three = function(number) {
-//   if (number % 3 == 0) {
-//   return alert("ping");
+// var pingPong = function(verified) {
+//   var getPingPong;
+//   if ((verified % 3 == 0) && (verified % 5 == 0)) {
+//     getPingPong = "ping-pong";
+//     console.log(pingPong);
+//   } else if (verified % 5 == 0) {
+//     getPingPong = "pong";
+//   } else if (verified % 3 == 0) {
+//     getPingPong = "ping";
 //   } else {
-//     return number
+//     getPingPong = verified;
 //   }
 // };
-// // // rule for ping-pong
-// var five = function(number) {
-//   if (number % 5 == 0){
-//   return alert("pong");
-//   } else {
-//     return number
-//   }
-// };
-// var allThree = function(number) {
-//   if ((number % 3 == 0) && (number % 5 == 0)) {
-//   return alert("ping-pong");
-//   } else {
-//     return number
-//   }
-// };
-var pingPong = function(number) {
-  if ((number % 3 == 0) && (number % 5 == 0)) {
-    return alert("ping-pong");
-  } else if (number % 5 == 0) {
-    return alert("pong");
-  } else if (number % 3 == 0) {
-    return alert("ping");
-  } else {
-    return number;
-  }
-};
-
-
-
-
 
 $(document).ready(function() {
   $("form#game").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("input#number").val());
-    var verified = validateInput(number);
-    // var ping = three(number);
-    // var pong = five(number);
-    var game = pingPong(number);
-    // var numbers = numberSplitter(verified);
-    // console.log(validateInput);
-    // $(".number").text(number);
-    // $(".roman").text(result);
-    //
-    // $("#result").show();
+    var list = numberList(number);
+    console.log(list);
+
+
+    // $(".number").text(game);
+
+    $(".number").text(list);
   });
 });
-    // var numbers = numberSplitter(verified);
-    // var onesPlace = onesIdentifier(numbers);
-    // var tensPlace = tensIdentifier(numbers);
-    // console.log(onesPlace);
-  //  var hundredsPlace = hundredsIdentifier(numbers);
-  //  var thousandsPlace = thousandsIdentifier(numbers);
-//   console.log(thousandsPlace + hundredsPlace + tensPlace + onesPlace);
-  //  var result = roman(thousandsPlace, hundredsPlace, tensPlace, onesPlace);
-  //  console.log(result);
