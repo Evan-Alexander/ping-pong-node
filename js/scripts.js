f9df861f4108d2deb0977523c4885528007f4986
Jason Brown <jasontbrown99@gmail.com>
2017-01-25 21:51:58 -0800

    Game now displays index numbers on screen.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,24 @@
-var numberList = function(number) {
-  var i = [];
-  for ( var i=1; i < number; i++ ) {
-    console.log(i)
-  }
-  return i;
-}
-
-
-// var pingPong = function(verified) {
-//   var getPingPong;
-//   if ((verified % 3 == 0) && (verified % 5 == 0)) {
-//     getPingPong = "ping-pong";
-//     console.log(pingPong);
-//   } else if (verified % 5 == 0) {
-//     getPingPong = "pong";
-//   } else if (verified % 3 == 0) {
-//     getPingPong = "ping";
-//   } else {
-//     getPingPong = verified;
-//   }
-// };
-
 $(document).ready(function() {
   $("form#game").submit(function(event) {
     event.preventDefault();
-    var number = parseInt($("input#number").val());
-    var list = numberList(number);
-    console.log(list);
+    var number1 = parseInt($("#number").val());
 
-    $("#result").text(number);
+    for(i = 0; i < number1; i++) {
+      $("#result").append(i + ', ');
+      console.log(i);
+    }
+    $("#result").css("display", "block");
+    // $("#result").text(number1);
   });
 });
+//
+// $(document).ready(function() {
+//   $("form#game").submit(function(event) {
+//     event.preventDefault();
+//     var number = parseInt($("input#number").val());
+//     var list = numberList(number);
+//     console.log(list);
+//
+//     $("#result").text(number);
+//   });
+// });
