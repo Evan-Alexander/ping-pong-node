f477420bf964e275d6b3239a191494d45cd2a836
Jason Brown <jasontbrown99@gmail.com>
2017-01-28 11:09:00 -0800

    add space to integer result for easier reading

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,13 @@ function pingPong(number1) {
   var pingArray = [];
   for ( i = 1; i <= number1; i++ ) {
     if (i % 15 === 0) {
-      pingArray.push("ping-pong");
+      pingArray.push(" ping-pong");
     } else if (i % 5 === 0) {
-      pingArray.push("pong");
+      pingArray.push(" pong");
     } else if (i % 3 === 0) {
-      pingArray.push("ping");
+      pingArray.push(" ping");
     } else {
-      pingArray.push(i);
+      pingArray.push(" " + i);
     }
   }
   console.log(pingArray);
@@ -23,7 +23,7 @@ $(document).ready(function() {
     event.preventDefault();
     var number1 = parseInt($("#number").val());
     var display = pingPong(number1);
-    $("#result").append(display + ', ');
+    $("#result").append(display + ',');
     console.log(display);
 
     $("#result").show();
