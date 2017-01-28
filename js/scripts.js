40d72c41a69060dcd4e31518434f228f59520471
Jason Brown <jasontbrown99@gmail.com>
2017-01-28 08:57:05 -0800

    edit html for click me. starting array structure in business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,29 @@
+
+
+function pingPong() {
+  var pingArray = [];
+  for (var i =1; i <= number; i++) {
+    if (i % 15 === 0) {
+      pingArray.push("ping-pong")
+
+    }
+  }
+  return pingArray
+}
+
+
+
+
 $(document).ready(function() {
   $("form#game").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("#number").val());
-
+    var display = pingPong(number1);
     for(i = 0; i < number1; i++) {
       $("#result").append(i + ', ');
       console.log(i);
     }
-    $("#result").css("display", "block");
+    $("#result").show(i);
     // $("#result").text(number1);
   });
 });
-//
-// $(document).ready(function() {
-//   $("form#game").submit(function(event) {
-//     event.preventDefault();
-//     var number = parseInt($("input#number").val());
-//     var list = numberList(number);
-//     console.log(list);
-//
-//     $("#result").text(number);
-//   });
-// });
